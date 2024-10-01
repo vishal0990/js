23a6326eea38e5d00116b871f05d28c037050cb6
vishal0990 <158131760+vishal0990@users.noreply.github.com>
2024-10-01 05:41:15 +0000

    conversion

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"
+
+
+console.log("hi");
+
+var a= "b"
+var b  = "c"
+var s =123123
+console.table([a,b,s]);
+
+// number 2 to power 53
+// bigint
+// string = ""
+// boolean  = false/true
+// null => standalone value / empty
+// undefined 
+// symbol =unique
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score  = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number (score)
+console.log( valueInNumber);
+
+// 33 =>"33"
+// "33adb"=> NaN
+// true =1 ; false =0;
+
+let boolValue =""
+
+let convertIntoBool =Boolean(boolValue)
+
+console.log(convertIntoBool);
+
+// 1=>true ; 0=> false
+// ""=> false; "vk" =>true
+
+
+let intNumber =33;
+let stringConversion =String(intNumber)
+
+console.log(stringConversion);
+
+console.log(typeof stringConversion);
